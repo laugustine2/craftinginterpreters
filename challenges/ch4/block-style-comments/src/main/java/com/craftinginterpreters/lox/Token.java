0d0d459bb3289c0fe619b1edd42ea4580e7e5d75
Leon Augustine <laugusti@mail.usf.edu>
2022-09-07 03:48:10 -0400

    Add solutions to chapter 4 challenges

---
+++ challenges/ch4/block-style-comments/src/main/java/com/craftinginterpreters/lox/Token.java
@@ -0,0 +1,37 @@
+package com.craftinginterpreters.lox;
+
+public class Token {
+
+    private final TokenType type;
+    private final String lexeme;
+    final Object literal;
+    final int line;
+
+    public Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    @Override
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getLexeme() {
+        return lexeme;
+    }
+
+    public Object getLiteral() {
+        return literal;
+    }
+
+    public int getLine() {
+        return line;
+    }
+}
