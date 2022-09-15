6db90fd646c43542f70e80061c17823d8894cb0e
Leon Augustine <laugusti@mail.usf.edu>
2022-09-14 22:29:41 -0400

    Add support for variable declarations and expressions to parser and interpreter

--- implementation/ch8/lox/src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ implementation/ch8/lox/src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -4,11 +4,15 @@ import com.craftinginterpreters.lox.Expr.Binary;
 import com.craftinginterpreters.lox.Expr.Grouping;
 import com.craftinginterpreters.lox.Expr.Literal;
 import com.craftinginterpreters.lox.Expr.Unary;
+import com.craftinginterpreters.lox.Expr.Variable;
 import com.craftinginterpreters.lox.Stmt.Expression;
 import com.craftinginterpreters.lox.Stmt.Print;
+import com.craftinginterpreters.lox.Stmt.Var;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+
+  private Environment environment = new Environment();
 
   void interpret(List<Stmt> statements) {
     try {
@@ -88,6 +92,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Object visitVariableExpr(Variable expr) {
+    return environment.get(expr.name);
+  }
+
+  @Override
   public Void visitExpressionStmt(Expression stmt) {
     evaluate(stmt.expression);
     return null;
@@ -97,6 +106,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   public Void visitPrintStmt(Print stmt) {
     Object value = evaluate(stmt.expression);
     System.out.println(stringify(value));
+    return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+    environment.define(stmt.name.lexeme, value);
     return null;
   }
 
--- implementation/ch8/lox/src/main/java/com/craftinginterpreters/lox/Parser.java
+++ implementation/ch8/lox/src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -16,9 +16,31 @@ public class Parser {
   List<Stmt> parse() {
     List<Stmt> statements = new ArrayList<>();
     while (!isAtEnd()) {
-      statements.add(statement());
+      statements.add(declaration());
     }
     return statements;
+  }
+
+  private Stmt declaration() {
+    try {
+      if (match(TokenType.VAR)) {
+        return varDeclaration();
+      }
+      return statement();
+    } catch (ParseError error) {
+      synchronize();
+      return null;
+    }
+  }
+
+  private Stmt varDeclaration() {
+    Token name = consume(TokenType.IDENTIFIER, "Expect variable name.");
+    Expr initializer = null;
+    if (match(TokenType.EQUAL)) {
+      initializer = expression();
+    }
+    consume(TokenType.SEMICOLON, "Expect ';' after variable declaration.");
+    return new Stmt.Var(name, initializer);
   }
 
   private Stmt statement() {
@@ -106,6 +128,9 @@ public class Parser {
     }
     if (match(TokenType.NUMBER, TokenType.STRING)) {
       return new Literal(previous().literal);
+    }
+    if (match(TokenType.IDENTIFIER)) {
+      return new Expr.Variable(previous());
     }
     if (match(TokenType.LEFT_PAREN)) {
       Expr expr = expression();
