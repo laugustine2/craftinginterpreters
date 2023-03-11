f5e8f7cce0642e5c57a552e59d1b7dd9415b7a64
Leon Augustine <laugusti@mail.usf.edu>
2023-03-11 15:21:51 -0500

    Add static error when accessing 'super' without no superclass

--- implementation/ch13/lox/src/main/java/com/craftinginterpreters/lox/Resolver.java
+++ implementation/ch13/lox/src/main/java/com/craftinginterpreters/lox/Resolver.java
@@ -26,7 +26,8 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   private enum ClassType {
     NONE,
-    CLASS
+    CLASS,
+    SUBCLASS
   }
 
   private ClassType currentClass = ClassType.NONE;
@@ -187,6 +188,11 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitSuperExpr(Expr.Super expr) {
+    if (currentClass == ClassType.NONE) {
+      Lox.error(expr.keyword, "Can't use 'super' outside of a class.");
+    } else if (currentClass != ClassType.SUBCLASS) {
+      Lox.error(expr.keyword, "Can't use 'super' in a clss with no superclass.");
+    }
     resolveLocal(expr, expr.keyword);
     return null;
   }
